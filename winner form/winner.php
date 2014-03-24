98dc3893467e21a79f2021a0822e8fb4fa1354af
Marty <janklovicsmarton@gmail.com>
2014-03-24 20:28:38 +0100

    Removed bullshit from php

--- winner form/winner.php
+++ winner form/winner.php
@@ -1,42 +1,25 @@
 <?php
 
-$rand = rand(10000000,99999999); //random number generator
+$rand = rand(10000000,99999999); //random number generator (8 digits)
 
 $myFile = "numbers.txt";	//append random number to file (with line break)
 $fh = fopen($myFile, 'a') or die("can't open file");
-$stringData = "$rand" . PHP_EOL;
-fwrite($fh, $stringData);
+fwrite($fh, $rand . PHP_EOL);
 fclose($fh);
 
 
+//mail()
+
+$message = "
+HTML tags are supported<br>
+Random number: <b>$rand<b>
+";
+
+$to = $_POST["to"];
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 $headers .= 'From: Lockville.hu <info@lockville.hu>' . "\r\n";
-$to = $_POST["to"];
-$message = "
-<html>
-<head>
-  <title>Birthday Reminders for August</title>
-</head>
-<body>
-  <p>Here are the birthdays upcoming in August!</p>
-  <table>
-    <tr>
-      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
-    </tr>
-    <tr>
-      <td>Joe</td><td>3rd</td><td>August</td><td>1970</td>
-    </tr>
-    <tr>
-      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
-    </tr>
-  </table>
-  
-													$rand
 
-  </body>
-</html>
-";
 
 $mail_status = mail("$to","Nyeremény",$message,$headers);
 
