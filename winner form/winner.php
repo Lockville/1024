e3c65047df4a806d5c8c4d1b067eb2323357d417
Marty <janklovicsmarton@gmail.com>
2014-03-24 20:41:53 +0100

    write email addresses to txt too

--- winner form/winner.php
+++ winner form/winner.php
@@ -1,10 +1,11 @@
 <?php
+$to = $_POST["to"];
 
 $rand = rand(10000000,99999999); //random number generator (8 digits)
 
 $myFile = "numbers.txt";	//append random number to file (with line break)
 $fh = fopen($myFile, 'a') or die("can't open file");
-fwrite($fh, $rand . PHP_EOL);
+fwrite($fh, $rand . " " . $to . PHP_EOL);
 fclose($fh);
 
 
@@ -15,7 +16,7 @@ HTML tags are supported<br>
 Random number: <b>$rand<b>
 ";
 
-$to = $_POST["to"];
+
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 $headers .= 'From: Lockville.hu <info@lockville.hu>' . "\r\n";
