fd7c649657e8706b5be3f3e6e6c7aee5522ddfc6
fonorobert <fonorobert@gmail.com>
2014-03-24 17:08:43 +0100

    Included Purecss for form and button styling
    -In styles: buttons.css, forms.css
    -Other changes: started building message markup in html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -124,7 +124,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
   this.bestContainer.textContent = bestScore;
 };
 
-var lockville = 'Na menj <a href="http://index.hu" target="_blank">INDEXet</a> olvasni Szoszo!'
+var lockville = 'Gratulálunk!<br/>Add meg az e-mail címed, hogy elküldhessük neked a kuponod!<br/><form method="post" action="#" role="form"><input type="email" placeholder="E-mail cím..." class="form-control"><button type="submit" class="btn button">Küldd a kupont!</button></form>';
 //$(document).ready(function(){
 //  $('p').append('Kattints <a href="http://index.hu" target="_blank">ide</a>')
 //})
