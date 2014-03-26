5df829eb76d1ceef55380eaa6dcb5b3b775951e0
fonorobert <fonorobert@gmail.com>
2014-03-26 14:15:53 +0100

    Set up email sending

--- winner.php
+++ winner.php
@@ -3,7 +3,9 @@ $to = $_POST["to"];
 
 $rand = rand(10000000,99999999); //random number generator (8 digits)
 
-$myFile = "numbers.txt";	//append random number to file (with line break)
+$rand = 'LV-'.$rand; //Add prefix to stop browsers from seeing number as phone no.
+
+$myFile = "srkszq1.txt";	//append random number to file (with line break)
 $fh = fopen($myFile, 'a') or die("can't open file");
 fwrite($fh, $rand . " " . $to . PHP_EOL);
 fclose($fh);
@@ -11,10 +13,38 @@ fclose($fh);
 
 //mail()
 
-$message = "
-HTML tags are supported<br>
-Random number: <b>$rand<b>
-";
+$message = '
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Lockville nyeremény kupon</title>
+</head>
+<body>
+  <div style="display: block; width: 80%; margin: auto; padding: 2%; padding-top: 0.5%; background-color: rgba(241, 196, 15, 0.15); border: 4px solid #c0392b; text-align: center; color: #43140f; font-family: sans-serif;">
+      <img src="http://lockville.hu/res/seal_small.png" alt="Lockville" style="display: block; margin: auto;" />
+      <h1 style="margin-top:0.3em">Lockville nyeremény kupon</h1>
+      <h2>A kupon sorszáma: '.$rand.'</h2>
+      <p>
+       Ez a kupon <strong>ingyenes</strong> részvételt biztosít <strong>1 fő</strong> részére a Lockville Kiszabadulós Kalandjátékban.
+      </p>
+    
+    
+    <hr/>
+    <p style="font-size: 0.75em; text-align: justify;">
+      <strong class="important">Megjegyzés:</strong> A nyeremény kupon a Lockville Kiszabadulós Kalandjáték pályájára érvényes. Egy csoportban egy kupon használható fel. Ez a kupon átruházható, de készpénzre nem váltható. Ez a kupon <strong>2014. május 15-ig</strong> használható fel.
+    </p>
+    <hr/>
+    <p>
+      További információk, nyitvatartás és elérhetőségek: <br/><a href="http://lockville.hu" target="_blank" style="color: #d14233;">www.lockville.hu</a>
+    </p>
+  </div>
+</body>
+</html>
+	
+
+';
 
 
 $headers  = 'MIME-Version: 1.0' . "\r\n";
@@ -32,7 +62,7 @@ if ($mail_status) { ?>
 }
 else { ?>
 	<script language="javascript" type="text/javascript">
-		window.location = '#';
+		window.location = 'index.html';
 	</script>
 <?php
 }
